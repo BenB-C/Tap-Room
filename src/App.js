b149f36be8bc522f2014c75a1057f9506eb259bd
bcauchon <bcauchon@gmail.com>
2019-09-06 11:33:44 -0700

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './Header.js';
 import KegList from './KegList.js';
+import NewKegForm from './NewKegForm.js';
+import Error404 from './Error404';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +11,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <KegList />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={KegList} />
+          <Route path='/newkeg' component={NewKegForm} />
+          <Route component={Error404} />
+        </Switch>
+      </Router>
     </div>
   );
 }
