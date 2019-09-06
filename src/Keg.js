03be6cc3a15b806852fe7ef48cfbe9c031448d40
bcauchon <bcauchon@gmail.com>
2019-09-06 12:03:35 -0700

    Add buttons to edit/delete a keg and sell a pint

--- src/Keg.js
+++ src/Keg.js
@@ -6,7 +6,10 @@ function Keg(props){
   return (
     <div className='Keg'>
       <p><span className='Keg-name'>{props.name}</span> - <span className='Keg-brand'>{props.brand}</span></p>
-      <p className='Keg-info'><span className='Keg-price'>Price: ${props.price}</span> <span className='Keg-alcoholContent'>ABV: {props.alcoholContent}%</span> <span className='Keg-pints'>Pints: {props.pints}</span></p>
+      <p className='Keg-info'><span className='Keg-price'>Price: ${props.price}</span> <span className='Keg-alcoholContent'>ABV: {props.alcoholContent}%</span> <span className='Keg-pints'>Pints: {props.pints}<button>Sell Pint</button></span></p>
+      
+      <button>Edit</button>
+      <button>Delete</button>
       <hr/>
     </div>
   );
