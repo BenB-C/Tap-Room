752eac4542e2322eb52d6e4d3916e296c7381469
bcauchon <bcauchon@gmail.com>
2019-09-06 14:08:32 -0700

    Remove unused variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Header.js';
 import KegList from './KegList.js';
 import NewKegForm from './NewKegForm.js';
 import Error404 from './Error404.js';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
