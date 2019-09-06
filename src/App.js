bfa5253e3886ffa374bf8365fd067585e9a2983a
bcauchon <bcauchon@gmail.com>
2019-09-06 13:05:00 -0700

    Change extension of Error404 from jsx to js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Header from './Header.js';
 import KegList from './KegList.js';
 import NewKegForm from './NewKegForm.js';
-import Error404 from './Error404';
+import Error404 from './Error404.js';
 import logo from './logo.svg';
 import './App.css';
 
