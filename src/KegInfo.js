f653d52f0c81d51397430427ecd984fbd48a56ac
bcauchon <bcauchon@gmail.com>
2019-09-13 15:02:51 -0700

    Adjust some styles

--- src/KegInfo.js
+++ src/KegInfo.js
@@ -5,12 +5,13 @@ import './KegInfo.css';
 
 function KegInfo(props) {
   const keg = props.keg;
-  const priceStyles = {
-    color: 'red',
-    fontWeight: 'bold'
+  const priceStyles = {}
+  if (keg.price > 5) {
+    priceStyles.fontWeight = 'bold';
   }
-  if (keg.price < 5) {
-    priceStyles.color = 'green';
+  const abvStyles = {}
+  if (keg.alcoholContent > 5) {
+    abvStyles.fontWeight = 'bold';
   }
 
   function handleClickOnDelete() {
@@ -21,8 +22,8 @@ function KegInfo(props) {
     <div className='KegInfo'>
       <div className='KegInfo-name'>{keg.name}</div>
       <div className='KegInfo-brand'>{keg.brand}</div>
-      <div className='KegInfo-price' style={priceStyles}>Price: ${keg.price}</div>
-      <div className='KegInfo-alcoholContent'>ABV: {keg.alcoholContent}%</div>
+      <div className='KegInfo-price' style={priceStyles}>${keg.price}</div>
+      <div className='KegInfo-alcoholContent' style={abvStyles}>{keg.alcoholContent}%</div>
       <div>
         <Link to={{
           pathname: '/editkeg',
